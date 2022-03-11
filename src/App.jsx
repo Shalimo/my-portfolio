08b334cbdedfa503450925b26f400410e3e3543d
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-03-12 00:23:32 +0300

    some fixes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css"
-import Header from "./components/header/header";
 import Introduction from "./components/introduction/introductions";
 import Projects from "./projects/projects";
 import Contacts from "./components/contacts/contacts";
